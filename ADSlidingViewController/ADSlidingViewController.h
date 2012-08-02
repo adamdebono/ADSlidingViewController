dd943e5ab10ce9c538527cd36204360c7c875959
Adam Debono <adam@joindup.com>
2012-08-02 19:35:34 +1000

    Initial Commit

---
+++ ADSlidingViewController/ADSlidingViewController.h
@@ -0,0 +1,13 @@
+//
+//  ADSlidingViewController.h
+//  ADSlidingViewController
+//
+//  Created by Adam Debono on 2/08/12.
+//  Copyright (c) 2012 Adam Debono. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADSlidingViewController : NSObject
+
+@end
---
+++ ADSlidingViewController/ADSlidingViewController.m
@@ -0,0 +1,13 @@
+//
+//  ADSlidingViewController.m
+//  ADSlidingViewController
+//
+//  Created by Adam Debono on 2/08/12.
+//  Copyright (c) 2012 Adam Debono. All rights reserved.
+//
+
+#import "ADSlidingViewController.h"
+
+@implementation ADSlidingViewController
+
+@end
